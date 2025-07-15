a54fa55cb2a833396cb46df11194a54062ca72aa
prabhu <palthyaprabhu143@gmai.com>
2025-07-15 20:31:04 +0530

    init commit

--- src/Two_D_Array_015.java
+++ src/Two_D_Array_015.java
@@ -4,9 +4,9 @@ public class Two_D_Array_015 {
     Scanner sc = new Scanner(System.in);
 
     static void printArray(int[][] array){
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
@@ -47,9 +47,113 @@ public class Two_D_Array_015 {
         printArray(mul);
     }
 
+    static void transposeMatrix(int[][] a,int r, int c){
+        int[][] ans = new int[c][r];
+        for (int i = 0; i < c; i++) { // cols
+            for (int j = 0; j < r; j++) { // rows
+                ans[i][j] = a[j][i];
+            }
+        }
+        System.out.println("Transposed Matrix :- ");
+        printArray(ans);
+    }
+
+    // Transpose in and store in  same array
+
+    static void transposeInPlace(int[][] matrix, int r, int c){
+    // Worked on only square matrix
+        for (int i = 0; i < c; i++) {
+            for (int j = i; j < r; j++) {
+                // Swap matrix[i][j], matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        printArray(matrix);
+    }
+
+    static void reverseArray(int [] array){
+        int i = 0;
+        int j = array.length - 1;
+        while (i<j){
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    // Rotate array
+
+    static void rotateMatrix(int[][] matrix, int n){
+        // Transpose
+        transposeInPlace(matrix,n,n);
+        // reverse each row of transpose matrix
+        for (int i = 0; i < n; i++) {
+            reverseArray(matrix[i]);
+        }
+
+    }
+
+    // Pascal Triangle
+
+    static void pascalTraingle(int n){
+        int[][] ans = new int[n][];
+
+        for (int i = 0; i < n; i++) {
+            // ith row has i + 1
+            ans[i] = new int[i+1];
+            // 1st and last element of every row is 1
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
+            }
+        }
+        printArray(ans);
+    }
+
+    // Spiral Order
+
+    static void spiralOrder(int[][] matrix, int r, int c){
+        int topRow = 0, bottomRow = r-1, leftCol = 0, rightCol = c-1;
+        int totalElements =  0;
+
+        while (totalElements < r * c){
+            // top row -> LeftCol to rightCol
+            for (int i = leftCol; i <= rightCol && totalElements < r * c; i++) {
+                System.out.print(matrix[topRow][i] + " ");
+                totalElements++;
+            }
+            topRow++;
+
+            // right -> topRow to BottomRow
+            for (int i = topRow; i <= bottomRow && totalElements < r * c; i++) {
+                System.out.print(matrix[i][rightCol] + " ");
+                totalElements++;
+            }
+            rightCol--;
+            // bottom -> rightCol to LeftCol
+            for (int i = rightCol; i >= leftCol && totalElements < r * c; i--) {
+                System.out.print(matrix[bottomRow][i] + " ");
+                totalElements++;
+            }
+            bottomRow--;
+            // leftCol -> buttomRow to TopRow
+            for (int i = bottomRow; i >= topRow && totalElements < r * c; i--) {
+                System.out.print(matrix[i][leftCol] + " ");
+                totalElements++;
+            }
+            leftCol++;
+        }
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Program started ");
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter number of rows and column of matrix 1");
         int r1 = sc.nextInt();
         int c1 = sc.nextInt();
@@ -60,8 +164,8 @@ public class Two_D_Array_015 {
             for (int j = 0; j < c1; j++) { // columns
                 a[i][j] = sc.nextInt();
             }
-
         }
+/*
 
         System.out.println("Enter number of rows and column of matrix 2");
         int r2 = sc.nextInt();
@@ -73,15 +177,27 @@ public class Two_D_Array_015 {
             for (int j = 0; j < c2; j++) { // columns
                 b[i][j] = sc.nextInt();
             }
-
         }
+
         System.out.println("Matrix one");
         printArray(a);
+
         System.out.println("Matrix two");
         printArray(b);
-//        printArray(a);
+        printArray(a);
         add(a,r1,c1,b,r2,c2);
-//        multiplication(a,r1,c1,b,r2,c2);
+        multiplication(a,r1,c1,b,r2,c2);
+        transposeMatrix(a,r1,c1);
+        System.out.println("transpose matrix");
+        transposeInPlace(a,r1,c1);
+        System.out.println("Rotated");
+        rotateMatrix(a,r1);
+        System.out.println("Enter the n value");
+        int n = sc.nextInt();
+        pascalTraingle(n);
 
+ */
+        System.out.println("Spiral Order");
+        spiralOrder(a,r1,c1);
     }
 }
